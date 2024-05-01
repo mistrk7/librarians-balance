d9d2d1f00d4c93b80e9f43307775b0628a7f2696
misterk7_- <merezkhaan@gmail.com>
2024-05-01 16:55:34 +0100

    Create book_price_calculator.py
    Small python script to understand and visualize how I plan to calculate costs soon

---
+++ book_price_calculator.py
@@ -0,0 +1,56 @@
+
+# Define the list of dictionaries representing objects
+#while True:
+book = [{"minecraft:efficiency":4},{"minecraft:unbreaking":3}]
+
+# Calculate the highest level
+lvl = max(obj[next(iter(obj))] for obj in book)
+
+# Calculate the number of objects
+stack = len(book)
+
+def cost(level):
+    for obj in book:
+        for key in obj.keys():
+            if key == "minecraft:mending":
+                return 52
+            if key in {"minecraft:channeling", "minecraft:aqua_affinity", "minecraft:silk_touch"}:
+                #EQUIV Level 5
+                return 48
+            if key in {"minecraft:flame", "minecraft:infinity", "minecraft:multishot"}:
+                #EQUIV Level 4
+                return 38
+            if key in {"minecraft:soul_speed", "minecraft:swift_sneak","minecraft:frost_walker","minecraft:wind_burst"}:
+                #These are all sealed books
+                #EQUIV Level 3
+                #Omitted: Breach & density because they are scalable
+                return 24
+    if level<4:
+        return level * 7
+    else:
+        return level * 10 - 2
+
+price = (cost(lvl))*stack
+blocks = round(price/9)
+if price > 63:
+    price_ = blocks
+    block_type = "minecraft:emerald_block"
+    #just to know what to do when implementing this
+
+# Print the results
+print("")
+print("Book:", book)
+print("")
+print("max lvl",lvl)
+print("stack",stack)
+print("")
+print("lvl 1 enchant", cost(1))
+print("lvl 2 enchant", cost(2))
+print("lvl 3 enchant", cost(3))
+print("lvl 4 enchant", cost(4))
+print("lvl 5 enchant", cost(5))
+print("")
+print("Emerald price:", price)
+print("Blocks:", blocks)
+print("")
+#print("Remainder:", remainder)
